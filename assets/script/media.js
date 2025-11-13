4111acb11dfd2b04ec0431365600201a7842f8d5
Hendras Laksono <hexa.official.cs@gmail.com>
2025-11-13 13:16:36 +0700

    Update media.js

--- assets/script/media.js
+++ assets/script/media.js
@@ -3,6 +3,6 @@
 const mediaLinks = {
   wa1: "https://wa.me/6285608564084",
   "wa-channel": "https://whatsapp.com/channel/0029VawrbYL1XquUo6yjJF1z",
-  facebook1: "https://facebook.com/wayangkulitsewon",
-  instagram1: "https://instagram.com/wayangkulitsewon",
+  facebook: "https://facebook.com/wayangkulitsewon",
+  instagram: "https://instagram.com/wayangkulitsewon",
 };
