f751881de983cff3669b73f74afe6047e2b3a4a5
Hendras Laksono <hexa.official.cs@gmail.com>
2025-11-13 13:23:47 +0700

    Update toko.js

--- assets/script/toko.js
+++ assets/script/toko.js
@@ -3,5 +3,5 @@
 const tokoLinks = {
   shopee: "https://shopee.co.id/wayangkulitsewon",
   padi: "https://padiumkm.id/store/68fb24ef35d64d875aa0549f",
-  tokopedia: "https://tokopedia.com/wayangkulitsewon"
+  tokopedia: "https://tokopedia.com/wayangkulitsewon",
 };
