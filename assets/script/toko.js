2fa16b084512d3b521571443963d96105e781f6a
Hendras Laksono <hexa.official.cs@gmail.com>
2025-11-14 04:53:10 +0700

    Update toko.js

--- assets/script/toko.js
+++ assets/script/toko.js
@@ -1,10 +1,11 @@
 // assets/script/toko.js
+// 🔹 Link marketplace resmi LaksanaCraft
 
 const tokoLinks = {
   shopee: "https://shopee.co.id/wayangkulitsewon",
-  padi: "https://padiumkm.id/store/68fb24ef35d64d875aa0549f",
   tokopedia: "https://tokopedia.com/wayangkulitsewon",
   blibli: "https://blibli.onelink.me/GNtk/3t7a8xdc",
-  toco: "https://toco.id/link/r4wCAhgEpuNWQDMg9o4zTH",
   lazada: "https://s.lazada.co.id/s.Z2CXKU",
+  padi: "https://padiumkm.id/store/68fb24ef35d64d875aa0549f",
+  toco: "https://toco.id/link/r4wCAhgEpuNWQDMg9o4zTH"
 };
