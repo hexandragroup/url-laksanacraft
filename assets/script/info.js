f0c92e36a712d1bf24f2aaef8207dc99193d1da8
Hendras Laksono <hexa.official.cs@gmail.com>
2025-11-14 04:51:05 +0700

    Create info.js

---
+++ assets/script/info.js
@@ -0,0 +1,27 @@
+// assets/script/info.js
+// 🔹 Informasi umum, kontak formal, profil, dan formulir
+
+const infoLinks = {
+  // 📍 Lokasi (Google Maps)
+  map: "https://maps.app.goo.gl/VwAHQdT2PSM8rMJo6",
+
+  // 📧 Email umum
+  email: "mailto:info@laksanacraft.my.id",
+
+  // 📩 Email otomatis (template isi)
+  "email-kerjasama": "mailto:info@laksanacraft.my.id?subject=Permintaan%20Kerja%20Sama&body=Nama%20Lengkap%3A%0ANama%20Perusahaan%2FOrganisasi%3A%0AAlamat%20Email%3A%0ANomor%20Telepon%2FWhatsApp%3A%0ADetail%20Kerja%20Sama%3A",
+  "email-order": "mailto:info@laksanacraft.my.id?subject=Pemesanan%20Produk&body=Nama%20Lengkap%3A%0ANomor%20Telepon%3A%0AAlamat%20Lengkap%3A%0ANama%20Produk%3A%0AUkuran%3A%0AJumlah%20Barang%3A",
+
+  // 🛍️ Contoh produk
+  produk: "https://laksanacraft.my.id/link/contoh",
+
+  // 👤 Profil perusahaan / pengrajin
+  profil: "https://link.laksanacraft.my.id/page/profile",
+
+  // 📝 Form internal
+  form: "https://link.laksanacraft.my.id/page/form",
+
+  // 🧾 Google Form
+  "form-google":
+    "https://docs.google.com/forms/d/e/1FAIpQLSfi8KVMGvkUovxsRPFChsfqW6nr083t58VhexU6mCCKZL2-BA/viewform"
+};
