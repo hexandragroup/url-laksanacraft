bdb2ff28dfa59e71f3418b5f012a6119f8280d6a
Hendras Laksono <hexa.official.cs@gmail.com>
2025-11-14 14:07:50 +0700

    Update media.js

--- assets/script/media.js
+++ assets/script/media.js
@@ -6,7 +6,7 @@ const mediaLinks = {
   wa: "https://wa.me/6285608564084",
   "wa-channel": "https://whatsapp.com/channel/0029VawrbYL1XquUo6yjJF1z",
   "wa-kerjasama": "https://wa.me/6285608564084?text=Halo%20Admin%2C%20saya%20ingin%20mengajukan%20kerja%20sama.%0ANama%20Lengkap%3A%20%0ANama%20Perusahaan%2FOrganisasi%3A%20%0AEmail%3A%20%0ANomor%20Telepon%3A%20%0ADetail%20Kerja%20Sama%3A%20",
-  "wa-order": "https://wa.me/6285608564084?text=Halo%20Admin%2C%20apakah%20produk%20ini%20tersedia%3F%0ANama%20Produk%3A%20%0AUkuran%3A%20%0AJumlah%3A%20",
+  "wa-order": "https://wa.me/6285608564084?text=Halo%20Admin%2VC%20apakah%20produk%20ini%20tersedia%3F%0ANama%20Produk%3A%20%0AUkuran%3A%20%0AJumlah%3A%20",
 
   // 🔵 Telegram
   telegram: "https://t.me/+6285608564084",
@@ -15,6 +15,7 @@ const mediaLinks = {
   // 🔗 Media Sosial
   facebook: "https://facebook.com/wayangkulitsewon",
   instagram: "https://instagram.com/wayangkulitsewon",
+  threads: "https://www.threads.net/@wayangkulitsewon",
   linkedin: "https://www.linkedin.com/in/hendraslaksono",
   tiktok: "https://tiktok.com/@wayangkulitsewon",
   twitter: "https://twitter.com/laksanacraft",
