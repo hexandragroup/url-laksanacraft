d3fa764b524b41241d8522d84e7afe8f68e8f8bb
Hendras Laksono <hexa.official.cs@gmail.com>
2025-11-21 21:21:29 +0700

    Update info.js

--- assets/script/info.js
+++ assets/script/info.js
@@ -1,5 +1,7 @@
 // assets/script/info.js
-// 🔹 Informasi umum, kontak formal, profil, dan formulir
+// ------------------------------------------------------
+// 🔹 Informasi umum, kontak, profil, dan formulir resmi
+// ------------------------------------------------------
 
 const infoLinks = {
   // 📍 Lokasi
@@ -10,11 +12,13 @@ const infoLinks = {
   email: "mailto:info@laksanacraft.my.id",
 
   // 📩 Email otomatis (template)
-  "email-kerjasama": "mailto:info@laksanacraft.my.id?subject=Permintaan%20Kerja%20Sama&body=Nama%20Lengkap%3A%0ANama%20Perusahaan%2FOrganisasi%3A%0AAlamat%20Email%3A%0ANomor%20Telepon%2FWhatsApp%3A%0ADetail%20Kerja%20Sama%3A",
+  "email-kerjasama":
+    "mailto:info@laksanacraft.my.id?subject=Permintaan%20Kerja%20Sama&body=Nama%20Lengkap%3A%0ANama%20Perusahaan%2FOrganisasi%3A%0AAlamat%20Email%3A%0ANomor%20Telepon%2FWhatsApp%3A%0ADetail%20Kerja%20Sama%3A",
 
-  "email-order": "mailto:info@laksanacraft.my.id?subject=Pemesanan%20Produk&body=Nama%20Lengkap%3A%0ANomor%20Telepon%3A%0AAlamat%20Lengkap%3A%0ANama%20Produk%3A%0AUkuran%3A%0AJumlah%20Barang%3A",
+  "email-order":
+    "mailto:info@laksanacraft.my.id?subject=Pemesanan%20Produk&body=Nama%20Lengkap%3A%0ANomor%20Telepon%3A%0AAlamat%20Lengkap%3A%0ANama%20Produk%3A%0AUkuran%3A%0AJumlah%20Barang%3A",
 
-  // 🔗 Link
+  // 🔗 Link umum
   link: "https://link.laksanacraft.my.id/links",
 
   // 👤 Profil
@@ -22,11 +26,15 @@ const infoLinks = {
 
   // 🛍️ Produk
   produk: "https://laksanacraft.my.id/link/contoh",
-
-  "produk-drive": "https://drive.google.com/drive/folders/12-r1ohZwagxnVdV1yU3DUTXhtsvA1nxS",
+  "produk-drive":
+    "https://drive.google.com/drive/folders/12-r1ohZwagxnVdV1yU3DUTXhtsvA1nxS",
 
   // 📝 Formulir
   form: "https://link.laksanacraft.my.id/page/form",
-
-  "form-google": "https://docs.google.com/forms/d/e/1FAIpQLSfi8KVMGvkUovxsRPFChsfqW6nr083t58VhexU6mCCKZL2-BA/viewform"
+  "form-google":
+    "https://docs.google.com/forms/d/e/1FAIpQLSfi8KVMGvkUovxsRPFChsfqW6nr083t58VhexU6mCCKZL2-BA/viewform"
 };
+
+// Debug loader
+console.log("INFO.JS LOADED");
+console.log(infoLinks);
