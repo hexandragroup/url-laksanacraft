4dca5f1edee4d9230903aa88d52330f84c4d0e1d
Hendras Laksono <hexa.official.cs@gmail.com>
2025-11-21 21:28:35 +0700

    Update info.js

--- assets/script/info.js
+++ assets/script/info.js
@@ -1,40 +1,30 @@
-// assets/script/info.js
-// ------------------------------------------------------
-// 🔹 Informasi umum, kontak, profil, dan formulir resmi
-// ------------------------------------------------------
-
-const infoLinks = {
-  // 📍 Lokasi
-  map: "https://maps.app.goo.gl/VwAHQdT2PSM8rMJo6",
-
-  // 📞 Kontak
-  telepon: "tel:+6285608564084",
-  email: "mailto:info@laksanacraft.my.id",
-
-  // 📩 Email otomatis (template)
-  "email-kerjasama":
-    "mailto:info@laksanacraft.my.id?subject=Permintaan%20Kerja%20Sama&body=Nama%20Lengkap%3A%0ANama%20Perusahaan%2FOrganisasi%3A%0AAlamat%20Email%3A%0ANomor%20Telepon%2FWhatsApp%3A%0ADetail%20Kerja%20Sama%3A",
-
-  "email-order":
-    "mailto:info@laksanacraft.my.id?subject=Pemesanan%20Produk&body=Nama%20Lengkap%3A%0ANomor%20Telepon%3A%0AAlamat%20Lengkap%3A%0ANama%20Produk%3A%0AUkuran%3A%0AJumlah%20Barang%3A",
-
-  // 🔗 Link umum
-  link: "https://link.laksanacraft.my.id/links",
-
-  // 👤 Profil
-  profil: "https://link.laksanacraft.my.id/page/profile",
-
-  // 🛍️ Produk
-  produk: "https://laksanacraft.my.id/link/contoh",
-  "produk-drive":
-    "https://drive.google.com/drive/folders/12-r1ohZwagxnVdV1yU3DUTXhtsvA1nxS",
-
-  // 📝 Formulir
-  form: "https://link.laksanacraft.my.id/page/form",
-  "form-google":
-    "https://docs.google.com/forms/d/e/1FAIpQLSfi8KVMGvkUovxsRPFChsfqW6nr083t58VhexU6mCCKZL2-BA/viewform"
+// assets/script/info.js  
+// 🔹 Informasi umum, kontak formal, profil, dan formulir  
+  
+const infoLinks = {  
+  // 📍 Lokasi  
+  map: "https://maps.app.goo.gl/VwAHQdT2PSM8rMJo6",  
+  
+  // 📞 Kontak  
+  telepon: "tel:+6285608564084",  
+  email: "mailto:info@laksanacraft.my.id",  
+  
+  // 📩 Email otomatis (template)  
+  "email-kerjasama": "mailto:info@laksanacraft.my.id?subject=Permintaan%20Kerja%20Sama&body=Nama%20Lengkap%3A%0ANama%20Perusahaan%2FOrganisasi%3A%0AAlamat%20Email%3A%0ANomor%20Telepon%2FWhatsApp%3A%0ADetail%20Kerja%20Sama%3A",  
+  
+  "email-order": "mailto:info@laksanacraft.my.id?subject=Pemesanan%20Produk&body=Nama%20Lengkap%3A%0ANomor%20Telepon%3A%0AAlamat%20Lengkap%3A%0ANama%20Produk%3A%0AUkuran%3A%0AJumlah%20Barang%3A",  
+  
+  // 🔗 Link  
+  link: "https://link.laksanacraft.my.id/links",  
+  
+  // 👤 Profil  
+  profil: "https://link.laksanacraft.my.id/page/profile",  
+  
+  // 🛍️ Produk  
+  produk: "https://laksanacraft.my.id/link/contoh",  
+  "produk-drive": "https://drive.google.com/drive/folders/12-r1ohZwagxnVdV1yU3DUTXhtsvA1nxS",  
+  
+  // 📝 Formulir  
+  form: "https://link.laksanacraft.my.id/page/form",  
+  "form-google": "https://docs.google.com/forms/d/e/1FAIpQLSfi8KVMGvkUovxsRPFChsfqW6nr083t58VhexU6mCCKZL2-BA/viewform",  
 };
-
-// Debug loader
-console.log("INFO.JS LOADED");
-console.log(infoLinks);
